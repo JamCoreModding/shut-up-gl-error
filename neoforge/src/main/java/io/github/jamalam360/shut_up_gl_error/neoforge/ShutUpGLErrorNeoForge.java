0c20717c92fa471563d84a8039c2622103f41e90
Jamalam <james@jamalam.tech>
2025-07-09 14:20:09 +0100

    feat: 1.21.1 update

---
+++ neoforge/src/main/java/io/github/jamalam360/shut_up_gl_error/neoforge/ShutUpGLErrorNeoForge.java
@@ -0,0 +1,11 @@
+package io.github.jamalam360.shut_up_gl_error.neoforge;
+
+import io.github.jamalam360.shut_up_gl_error.ShutUpGLError;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ShutUpGLError.MOD_ID)
+public class ShutUpGLErrorNeoForge {
+    public ShutUpGLErrorNeoForge() {
+        ShutUpGLError.init();
+    }
+}
