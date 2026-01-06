331ad7c9c63dbfb4969d18d4a16760ad707c1bb7
Jamalam <james@jamalam.tech>
2026-01-06 16:11:19 +0000

    feat: update to 1.21.11

--- common/src/main/java/io/github/jamalam360/shut_up_gl_error/ShutUpGLError.java
+++ common/src/main/java/io/github/jamalam360/shut_up_gl_error/ShutUpGLError.java
@@ -7,7 +7,7 @@ import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import io.github.jamalam360.jamlib.config.ConfigManager;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class ShutUpGLError {
 	public static final String MOD_ID = "shut_up_gl_error";
@@ -15,7 +15,7 @@ public class ShutUpGLError {
 
 	public static void init() {
 		if (Platform.isDevelopmentEnvironment()) {
-			KeyMapping.Category category = KeyMapping.Category.register(ResourceLocation.fromNamespaceAndPath(MOD_ID, "shut_up_gl_error"));
+			KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "shut_up_gl_error"));
 			KeyMapping keyMapping = new KeyMapping("shut_up_gl_error.test", InputConstants.Type.KEYSYM, InputConstants.KEY_G, category);
 			KeyMappingRegistry.register(keyMapping);
 
